9aae8789ba4372c8928e13b55854fb44db014a51
PKFireBarry <Barry0719@gmail.com>
2023-03-08 05:51:13 -0500

    Version 0.0.4
    Feature: added delete button

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { db } from '../firebase'
-import { collection, addDoc, serverTimestamp, onSnapshot, query, where, orderBy } from 'firebase/firestore'
+import { collection, addDoc, serverTimestamp, onSnapshot, query, where, orderBy, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 
 import { auth } from '../firebase'
 import { signOut } from 'firebase/auth';
@@ -12,6 +12,7 @@ function Chat(props) {
   
     const [messages, setMessages] = React.useState([]);
     const [newMessage, setNewMessage] = React.useState('');
+    const [editMessage, setEditMessage] = React.useState(null);
   
     const messagesRef = collection(db, 'messages');
   
@@ -47,6 +48,43 @@ function Chat(props) {
       }
     };
   
+    const handleEdit = async (messageId) => {
+      if (editMessage !== null) return;
+      const message = messages.find((m) => m.id === messageId);
+      if (!message) return;
+      setEditMessage(message);
+    };
+  
+    const handleUpdate = async (e) => {
+      e.preventDefault();
+      if (editMessage === null) return;
+      try {
+        await updateDoc(doc(db, 'messages', editMessage.id), {
+          message: newMessage,
+          createdAt: serverTimestamp(),
+        });
+        setEditMessage(null);
+        setNewMessage('');
+      } catch (error) {
+        console.error('Error updating document: ', error);
+      }
+    };
+  
+    const handleDelete = async (messageId) => {
+        try {
+          const message = messages.find((m) => m.id === messageId);
+          if (!message) return;
+          if (auth.currentUser.displayName !== message.user) {
+            console.error('User not authorized to delete this message');
+            return;
+          }
+          await deleteDoc(doc(db, 'messages', messageId));
+        } catch (error) {
+          console.error('Error deleting document: ', error);
+        }
+      };
+      
+  
     const signOutUser = async () => {
       try {
         await signOut(auth);
@@ -57,28 +95,31 @@ function Chat(props) {
         console.log(error);
       }
     };
-  
 
 
     return (
-<div className="h-screen flex flex-col bg-slate-600 pt-4 p-6 justify-center items-center ">
-  <h1 className="text-4xl text-white  mb-8 font-bold">{rooms.toUpperCase()}</h1>
-  <div className="flex flex-col w-full flex-grow bg-white sticky rounded-lg overflow-y-hidden">
+<div className=" flex flex-col bg-slate-600 p-8 justify-center items-center ">
+  <h1 className="text-4xl text-white  mb-8 font-bold">Room: {rooms.toUpperCase()} </h1>
+  <div className="flex flex-col w-full h-screen flex-grow bg-white sticky rounded-lg overflow-y-hidden">
   {messages.map(({ id, message, user, userImage, createdAt }) => (
     <div key={id} className="flex flex-col p-4 border-b border-gray-200">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <img className="w-12 h-12 rounded-full mr-4" src={userImage} alt="" />
+          <img className="w-12 h-12 rounded-full  mr-4" src={userImage} alt="" />
           <div>
             <p className="font-medium">{user}</p>
             <p className="text-sm text-gray-500">{new Date(createdAt?.seconds * 1000).toLocaleString()}</p>
           </div>
         </div>
         <div className="flex items-center">
-          <button className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-2 py-1 rounded-full mr-2 focus:outline-none">
+          <button onClick={
+            () => handleEdit(id)
+          } className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-2 py-1 rounded-full mr-2 focus:outline-none">
             Edit
           </button>
-          <button className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-full focus:outline-none">
+          <button onClick={
+            () => handleDelete(id)
+          } className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-full focus:outline-none">
             Delete
           </button>
         </div>
@@ -97,7 +138,7 @@ function Chat(props) {
       placeholder="Enter your message"
     />
     <button
-      className="bg-purple-600 hover:bg-purple-700 text-white rounded-full py-3 px-6 focus:outline-none"
+      className="bg-blue-600 hover:bg-sky-500 text-white rounded-full py-3 px-6 focus:outline-none"
       type="submit"
     >
       Send
